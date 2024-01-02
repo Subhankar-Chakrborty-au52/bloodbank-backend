2cb07b77286d2a60cd0c4a252ff3f66a46b51b11
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-01-02 17:48:39 +0530

    nnnn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// importing dependencies
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
-const initdb = require("./config/dbconnect");
-const app = express();
-const path = require("path");
-initdb();
+// // importing dependencies
+// const express = require("express");
+// require("dotenv").config();
+// const cors = require("cors");
+// const initdb = require("./config/dbconnect");
+// const app = express();
+// const path = require("path");
+// initdb();
 
-// middlewares
-app.use(express.json());
-app.use(cors());
+// // middlewares
+// app.use(express.json());
+// app.use(cors());
 
-app.get("/", (_req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
+// app.get("/", (_req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
 
-//routes
-app.use("/api/v1/auth", require("./routes/authroutes"));
-app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
-app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
-app.use("/api/v1/admin", require("./routes/adminRoutes"));
+// //routes
+// app.use("/api/v1/auth", require("./routes/authroutes"));
+// app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+// app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+// app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 // STATIC FOLDER
 // app.use(express.static(path.join(__dirname, "./client/build")));
@@ -34,30 +34,36 @@ app.use("/api/v1/admin", require("./routes/adminRoutes"));
 // })
 
 // Port and Server starting
+// const PORT = 4000;
+// app.listen(PORT, () => {
+//   console.log(
+//     `Node server running in ${process.env.DEV_MODE} Mode on Port ${process.env.PORT}`
+//   );
+// });
+
+// index.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
 const PORT = 4000;
+
+app.use(express.json());
+app.use(cors());
+
 app.listen(PORT, () => {
-  console.log(
-    `Node server running in ${process.env.DEV_MODE} Mode on Port ${process.env.PORT}`
-  );
+  console.log(`API listening on PORT ${PORT} `);
 });
 
-// // index.js
-// const express = require("express");
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
-// const app = express();
-// const PORT = 4000;
-
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hey this is my API running 🥳");
-// });
-
-// app.get("/about", (req, res) => {
-//   res.send("This is my about route..... ");
-// });
+//routes
+app.use("/api/v1/auth", require("./routes/authroutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
 // Export the Express API
 module.exports = app;
